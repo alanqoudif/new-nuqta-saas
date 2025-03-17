ece73e91affff51992e78dc315d5c6d069ffcd39
faisal al anqoudi <54464056+alanqoudif@users.noreply.github.com>
2025-03-17 23:45:53 +0400

    Add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,43 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/bolt.diy',
-        destination: '/bolt.diy/index.html',
-      },
-      {
-        source: '/bolt.diy/:path*',
-        destination: '/bolt.diy/:path*',
-      },
-      {
-        source: '/bolt.diy/assets/:path*',
-        destination: '/bolt.diy/assets/:path*',
-      },
-    ];
+  images: {
+    domains: ['nkdohpluytajrtflgagp.supabase.co', 'raw.githubusercontent.com'],
   },
-  async headers() {
-    return [
-      {
-        source: '/bolt.diy/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
-};
+}
 
-module.exports = nextConfig; 
+module.exports = nextConfig
